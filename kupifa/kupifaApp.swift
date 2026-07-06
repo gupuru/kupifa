a2f63a38871c1f864bb34152d97dc402c168b850
Kohei Niimi <7896734+gupuru@users.noreply.github.com>
2026-07-07 02:11:22 +0900

    Initial Commit

---
+++ kupifa/kupifaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kupifaApp.swift
+//  kupifa
+//
+//  Created by kohei on 2026/07/07.
+//
+
+import SwiftUI
+
+@main
+struct kupifaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kupifaTests/kupifaTests.swift
@@ -0,0 +1,18 @@
+//
+//  kupifaTests.swift
+//  kupifaTests
+//
+//  Created by kohei on 2026/07/07.
+//
+
+import Testing
+
+struct kupifaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
